ff37729e0ccd3a440e0fa65b2c9ebce1e4431a88
5kilian <5kilian@informatik.uni-hamburg.de>
2018-01-28 17:30:51 +0100

    Fix positions of arc-connections

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import RnwParser from './index';
 
 test('testRank', async t => {
-    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/test.rnw'));
+    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/kink.rnw'));
 
     t.is((await res)[0].size, 5);
 });
