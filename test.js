ed88dc56487d91db679e10ad688e781f710d5742
5kilian <5kilian@informatik.uni-hamburg.de>
2018-01-29 20:01:48 +0100

    Implement references for arrows, virtual places and text

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 import test from 'ava';
+import util from 'util';
 import RnwParser from './index';
 
 test('testRank', async t => {
-    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/untitled.rnw')).then(r => {
-        // console.log(r);
+    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/inscription.rnw')).then(r => {
+        console.log(util.inspect(r, false, null));
         return r;
     });
 
-    t.is((await res).size, 26);
+    t.is((await res).size, 10);
 });
