4806a9372ca6720bb6a6ce9d4d6c846c9afbf151
5kilian <5kilian@informatik.uni-hamburg.de>
2018-01-29 23:21:06 +0100

    Refactor parse row param

--- test.js
+++ test.js
@@ -2,11 +2,20 @@ import test from 'ava';
 import util from 'util';
 import RnwParser from './index';
 
-test('testRank', async t => {
+test('testRnw', async t => {
     let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/inscription.rnw')).then(r => {
-        console.log(util.inspect(r, false, null));
+        // console.log(util.inspect(r, false, null));
         return r;
     });
 
     t.is((await res).size, 10);
+});
+
+test('testPnml', async t => {
+    let res = Promise.resolve(RnwParser.readPnml('./resources/test-files/test.pnml')).then(r => {
+        console.log(util.inspect(r, false, null));
+        return r;
+    });
+
+    t.is((await res).size, 1);
 });
