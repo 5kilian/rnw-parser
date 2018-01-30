92f7db1aef935551ab3525f6c32bbe5c6d732220
5kilian <5kilian@informatik.uni-hamburg.de>
2018-01-30 01:05:44 +0100

    Add support for pnml drawings

--- test.js
+++ test.js
@@ -3,19 +3,19 @@ import util from 'util';
 import RnwParser from './index';
 
 test('testRnw', async t => {
-    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/inscription.rnw')).then(r => {
-        // console.log(util.inspect(r, false, null));
-        return r;
-    });
-
-    t.is((await res).size, 10);
-});
-
-test('testPnml', async t => {
-    let res = Promise.resolve(RnwParser.readPnml('./resources/test-files/test.pnml')).then(r => {
+    let res = Promise.resolve(RnwParser.readRnw('./resources/test-files/untitled.rnw')).then(r => {
         console.log(util.inspect(r, false, null));
         return r;
     });
 
-    t.is((await res).size, 1);
+    t.is((await res).size, (await res).figures.length);
+});
+
+test('testPnml', async t => {
+    let res = Promise.resolve(RnwParser.readPnml('./resources/test-files/untitled.pnml')).then(r => {
+        console.log(util.inspect(r, false, null));
+        return r;
+    });
+
+    t.is((await res).size, 15);
 });
